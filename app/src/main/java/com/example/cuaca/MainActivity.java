27748c79169bd84f9cf8c18528b30babcfea8c8a
Vitly7 <111552374+Vitly7@users.noreply.github.com>
2024-06-27 19:38:32 +0700

    done

--- app/src/main/java/com/example/cuaca/MainActivity.java
+++ app/src/main/java/com/example/cuaca/MainActivity.java
@@ -1,24 +1,69 @@
 package com.example.cuaca;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView _recyclerView1;
+    private SwipeRefreshLayout _swipeRefreshLayout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        _recyclerView1 = (RecyclerView) findViewById(R.id.recyclerView1);
+        _swipeRefreshLayout1 = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout1);
+
+        initRecyclerView1();
+        initSwipeRefreshLayout();
+    }
+    private void initSwipeRefreshLayout()
+    {
+        _swipeRefreshLayout1.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                initRecyclerView1();
+                _swipeRefreshLayout1.setOnRefreshing(false);
+            }
+        });
+    }
+
+    private void initRecyclerView1() {
+        String url = "https://api.openweathermap.org/data/2.5/forecast?id=1630789&appid=b7782ad2b41bcbdfec28f79bb9d4cd95";
+        AsyncHttpClient ahc = new AsyncHttpClient();
+
+        ahc.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Gson gson = new Gson();
+                RootModel rm = gson.fromJson(new String(responseBody), RootModel.class);
+                RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
+                CuacaAdapter ca = new CuacaAdapter(rm);
+
+                _recyclerView1.setLayoutManager(lm);
+                _recyclerView1.setAdapter(ca);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
